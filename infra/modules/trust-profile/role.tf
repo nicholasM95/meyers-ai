402c50e8c376e31a898487b387a746756dac7e8e
Nicholas Meyers <nicholas.meyers@hotmail.com>
2026-01-10 10:41:01 +0100

    chore: add iam policies

---
+++ infra/modules/trust-profile/role.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_managed_policy" "app_policy" {
+  name = "MeyersAIPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "bedrock:InvokeModel",
+          "bedrock:InvokeModelWithResponseStream"
+        ]
+        Resource = "arn:aws:bedrock:*::foundation-model/amazon.titan-text-lite-v1"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "app_iam_role" {
+  name = "MeyersAIPolicy"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::896918338968:oidc-provider/oidc-discovery.nicholasmeyers.be"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          ForAnyValue:StringEquals = {
+          "oidc-discovery.nicholasmeyers.be:aud" = "sts.amazonaws.com"
+          }
+          StringEquals = {
+            "oidc-discovery.nicholasmeyers.be:sub" = "spiffe://nicholasmeyers.be/ns/meyers-ai/sa/meyers-ai"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = aws_iam_managed_policy.app_policy.arn
+}
--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -1,8 +1,17 @@
+module "trust_profile" {
+  source     = "../modules/trust-profile"
+  name       = var.name
+  namespace  = var.namespace
+}
+
 module "application" {
-  source           = "git::https://github.com/nicholasM95/terraform-modules.git//modules/k8s-helm-release?ref=v1.12.40"
-  image_tag        = var.image_tag
-  application_name = var.name
-  namespace_name   = var.namespace
-  helm_path        = "../../application"
-  docker_config    = var.docker_config
+  source            = "git::https://github.com/nicholasM95/terraform-modules.git//modules/k8s-helm-release?ref=v1.10.10"
+  image_tag         = var.image_tag
+  application_name  = var.name
+  namespace_name    = var.namespace
+  helm_path         = "../../application"
+  docker_config     = var.docker_config
+  aws_trust_profile = module.trust_profile.aws_trust_profile
+  aws_trust_anchor  = module.trust_profile.aws_trust_anchor
+  aws_role          = module.trust_profile.aws_role
 }
