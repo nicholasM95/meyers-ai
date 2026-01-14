03a522a1cd38ac2e96a90da3310ee62394c5fa60
Nicholas Meyers <nicholas.meyers@hotmail.com>
2026-01-14 23:37:43 +0100

    chore: update bean config

--- backend/application/src/main/java/be/nicholasmeyers/meyersai/application/Application.java
+++ backend/application/src/main/java/be/nicholasmeyers/meyersai/application/Application.java
@@ -1,6 +1,7 @@
 package be.nicholasmeyers.meyersai.application;
 
 import org.springframework.ai.model.bedrock.cohere.autoconfigure.BedrockCohereEmbeddingAutoConfiguration;
+import org.springframework.ai.model.bedrock.converse.autoconfigure.BedrockConverseProxyChatAutoConfiguration;
 import org.springframework.ai.model.bedrock.titan.autoconfigure.BedrockTitanEmbeddingAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {
         BedrockCohereEmbeddingAutoConfiguration.class,
-        BedrockTitanEmbeddingAutoConfiguration.class
+        BedrockTitanEmbeddingAutoConfiguration.class,
+        BedrockConverseProxyChatAutoConfiguration.class
 })
 @ComponentScan(basePackages = {"be.nicholasmeyers.meyersai.chat.adpater"})
 public class Application {
