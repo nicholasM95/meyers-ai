556907e8bcd33686df450af056d39d1487452eeb
Nicholas Meyers <nicholas.meyers@hotmail.com>
2026-01-14 22:43:56 +0100

    chore: update spiffe config

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -1,13 +1,13 @@
 module "trust_profile" {
-  source    = "../modules/trust-profile"
-  name      = "var.name"
+  source = "../modules/trust-profile"
+  name   = "var.name"
 }
 
 module "application" {
-  source            = "git::https://github.com/nicholasM95/terraform-modules.git//modules/k8s-helm-release?ref=v1.10.10"
-  image_tag         = var.image_tag
-  application_name  = var.name
-  namespace_name    = var.namespace
-  helm_path         = "../../application"
-  docker_config     = var.docker_config
+  source           = "git::https://github.com/nicholasM95/terraform-modules.git//modules/k8s-helm-release?ref=v1.10.10"
+  image_tag        = var.image_tag
+  application_name = var.name
+  namespace_name   = var.namespace
+  helm_path        = "../../application"
+  docker_config    = var.docker_config
 }
