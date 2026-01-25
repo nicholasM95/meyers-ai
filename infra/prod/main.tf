1f30d96315101f807e072c56b3d8a2f04278f181
Nicholas Meyers <nicholas.meyers@hotmail.com>
2026-01-25 14:08:48 +0100

    chore: add frontend build

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -10,3 +10,15 @@ module "application" {
   helm_path        = "../../application"
   docker_config    = var.docker_config
 }
+
+module "static_website" {
+  source                  = "git::https://github.com/nicholasM95/terraform-modules.git//modules/static-website?ref=v1.12.74"
+  domain_name             = "nicholasmeyers.be"
+  sub_domain_name         = "ai"
+  project_name            = "ai-nicholasmeyers-be"
+  website_host            = "ai.nicholasmeyers.be"
+  website_path            = "../../dist"
+  content_security_policy = var.content_security_policy
+  permission_policy       = var.permission_policy
+  web_acl_id              = "arn:aws:wafv2:us-east-1:896918338968:global/webacl/waf-cloudfront/6fea776f-ac4b-4be3-b959-df5acdfe8e35"
+}
