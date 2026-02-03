74b9bf54e315df6ccb1ad5ffde9aefc73e902a10
Nicholas Meyers <nicholas.meyers@hotmail.com>
2026-02-03 19:29:24 +0100

    chore: add custom ToolCallbackProvider

--- backend/chat-adapter/src/main/java/be/nicholasmeyers/meyersai/chat/adpater/config/McpClientConfig.java
+++ backend/chat-adapter/src/main/java/be/nicholasmeyers/meyersai/chat/adpater/config/McpClientConfig.java
@@ -4,6 +4,9 @@ import io.modelcontextprotocol.client.McpClient;
 import io.modelcontextprotocol.client.McpSyncClient;
 import io.modelcontextprotocol.client.transport.HttpClientSseClientTransport;
 import io.modelcontextprotocol.spec.McpSchema;
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,6 +22,13 @@ public class McpClientConfig {
 
         return McpClient.sync(transport)
                 .clientInfo(new McpSchema.Implementation("bank-client", "1.0.0"))
+                .build();
+    }
+
+    @Bean
+    public ToolCallbackProvider mcpToolCallbackProvider(@Qualifier("bank-server") McpSyncClient bankServerClient) {
+        return SyncMcpToolCallbackProvider.builder()
+                .mcpClients(bankServerClient)
                 .build();
     }
 
